ad12854196b2d5f3966c7a769733b86facbea976
SamuelCaitano <83779077+SamuelCaitano@users.noreply.github.com>
2023-08-02 14:48:23 -0300

    criando váriaveis e printando no console!

---
+++ aula-003/ex/index.js
@@ -0,0 +1,5 @@
+var firstName = 'Samuel'
+var language = 'JavaScript'
+var hour = 10
+
+console.log("My name's " + firstName + ". I'm learning " + language + " at " + hour + "AM.")
---
+++ aula-003/index.js
@@ -0,0 +1,5 @@
+var firstName = 'Samuel'
+var surname = 'Caitano'
+
+console.log(firstName + ' ' + surname) 
+console.log(23, 15.5, firstName)
