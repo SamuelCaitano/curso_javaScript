526200c9dc3eb5c107ec71e4e3e5bd0724ef1d91
SamuelCaitano <83779077+SamuelCaitano@users.noreply.github.com>
2023-08-02 15:09:39 -0300

    entendendo sobre comentários, e como ele nos auxilia no entendimento do código(documentação)

---
+++ aula-004/index.js
@@ -0,0 +1,12 @@
+// Comentário de única linha
+console.log('Olá mundo!'); // Comentários não serão exibidos no terminal
+
+/*
+    Comentário de multiplas linhas
+*/
+
+/*
+*
+* Comentário de documentação
+*
+*/
