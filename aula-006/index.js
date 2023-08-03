1cd058f1d430eff8c09c93a1854d2c545051dc11
SamuelCaitano <83779077+SamuelCaitano@users.noreply.github.com>
2023-08-02 23:14:49 -0300

    atribuindo um  novo valor a uma váriavel

---
+++ aula-006/index.js
@@ -0,0 +1,8 @@
+let nome = 'João'; // String
+
+nome = "Matheus"
+
+console.log(nome, 'nasceu em 1995');
+console.log(nome, 'casou-se com Maria')
+console.log('Maria teve um filho de', nome)
+console.log('O filho de', nome,'e Maria é Eduardo')
