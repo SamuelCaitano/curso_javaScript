2bd9c145339c4bddd80f8c40994f1014934b2362
SamuelCaitano <83779077+SamuelCaitano@users.noreply.github.com>
2023-08-02 23:17:04 -0300

    váriaveis do tipo CONST

---
+++ aula-007/script.js
@@ -0,0 +1,9 @@
+// Constantes devem ser inicializadas
+// O valor de uma constante não pode ser modificado 
+// Variaveis devem conter nome alto explicatovos
+
+const MESES = 12
+const JANEIRO = 31
+
+console.log('Janeiro tem', JANEIRO, 'dias')
+console.log('1 ano tem', MESES, 'meses') 
